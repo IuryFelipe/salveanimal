dacf9dda183a9d19b9b7f4764eeee872d2fce497
Iury <iurycomputacao@gmail.com>
2020-08-05 17:50:38 -0300

    Add files via upload

--- js/doacao.js
+++ js/doacao.js
@@ -1,1 +1,18 @@
+function a(){
+    console.log("chamou função")
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('email').value;
+    var sobrevoce = document.getElementById('sobrevoce').value;
+    console.log(nome)
+    if(nome==null || nome==undefined || nome=="" || email==null || email==undefined || email=="" || sobrevoce==null || sobrevoce==undefined || sobrevoce==""){
+        alert("Preencha todos os campos!");
+    }else{
+        alert("Solicitação enviada com sucesso, "+nome+" :)");
+        
+    }
+   
+}
 
+function doar(){    
+    alert("Obrigado! Sua doação foi realizada com sucesso!"+valor);
+}
